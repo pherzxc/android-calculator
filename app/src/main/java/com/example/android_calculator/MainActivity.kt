772b08f6f95790ec264a348117aca6de02d7b4fc
John Christopher B. Mata <jmata@SVI.COM.PH>
2019-02-08 13:37:33 +0800

    Android Calculator App using Kotlin

---
+++ app/src/main/java/com/example/android_calculator/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.android_calculator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var tvExpression : TextView
+    lateinit var tvResult : TextView
+    lateinit var tvPlus : TextView
+    lateinit var tvMinus : TextView
+    lateinit var tvMultiply : TextView
+    lateinit var tvDivide : TextView
+    lateinit var tvEquals : TextView
+    lateinit var tvClear : TextView
+    lateinit var tvOpen : TextView
+    lateinit var tvClose : TextView
+    lateinit var tvNine : TextView
+    lateinit var tvEight : TextView
+    lateinit var tvSeven : TextView
+    lateinit var tvSix : TextView
+    lateinit var tvFive : TextView
+    lateinit var tvFour : TextView
+    lateinit var tvThree : TextView
+    lateinit var tvTwo : TextView
+    lateinit var tvOne : TextView
+    lateinit var tvZero : TextView
+    lateinit var tvDot : TextView
+    lateinit var tvBack : ImageView
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun append
+
+
+}
